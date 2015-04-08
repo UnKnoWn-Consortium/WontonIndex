a1122d17d31baf202edb0a3b252cd1d7779a823b
UnKnoWn <thomas123456789000@gmail.com>
2015-04-09 00:40:13 +0800

    Finalizing frontend and backend

--- src/script/script.php
+++ src/script/script.php
@@ -1,7 +1,7 @@
-﻿<?php
-	$dbName = '';
-	$dbUsername = '';
-	$dbPassword = '';
+<?php
+	$dbName = "";
+	$dbUsername = "";
+	$dbPassword = "";
 	$reCAPTCHA_key = "";
 
 	$requestIP = $_SERVER['REMOTE_ADDR']; 
@@ -9,13 +9,19 @@
 	$date = $_POST["date"]; 
 	$reCAPTCHA = $_POST["reCAPTCHAres"]; 
 	$image = $_POST['imageURL']; 
+	$restaurantName = $_POST['restaurantName'];
+	$price = $_POST['price'];
+	$numWonton = $_POST['numWonton'];
+	$rating = $_POST['rating'];
 	$EXIF = json_decode($_POST['EXIF']);
 	$EXIFmanufacturer = $EXIF->manufacturer;
 	$EXIFmodel = $EXIF->model;
-	$EXIFlat = $EXIF->gps->latitudeRef." ".$EXIF->gps->latitude[0]."º ".$EXIF->gps->latitude[1]."' ".$EXIF->gps->latitude[2].'"';
-	$EXIFlong = $EXIF->gps->longitudeRef." ".$EXIF->gps->longitude[0]."º ".$EXIF->gps->longitude[1]."' ".$EXIF->gps->longitude[2].'"';
-	$EXIFalt = $EXIF->gps->altitudeRef.$EXIF->gps->altitude; 
-	$EXIFtimestamp = $EXIF->gps->timeStamp[0].":".$EXIF->gps->timeStamp[1].":".$EXIF->gps->timeStamp[2];
+	if ($EXIF->gps !== NULL){
+		$EXIFlat = $EXIF->gps->latitudeRef." ".$EXIF->gps->latitude[0]."º ".$EXIF->gps->latitude[1]."' ".$EXIF->gps->latitude[2].'"';
+		$EXIFlong = $EXIF->gps->longitudeRef." ".$EXIF->gps->longitude[0]."º ".$EXIF->gps->longitude[1]."' ".$EXIF->gps->longitude[2].'"';
+		$EXIFalt = $EXIF->gps->altitudeRef.$EXIF->gps->altitude; 
+		$EXIFtimestamp = $EXIF->gps->timeStamp[0].":".$EXIF->gps->timeStamp[1].":".$EXIF->gps->timeStamp[2];
+	}
 	
 	$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$reCAPTCHA_key."&response=".$reCAPTCHA."&remoteip=".$requestIP);
 	if (strpos($response, 'true') !== false){$reCAPTCHApass = 1;}else{$reCAPTCHApass = 0;}
@@ -34,8 +40,8 @@
 	$db = new mysqli('localhost', $dbUsername, $dbPassword, $dbName);
 	if($db->connect_errno > 0){die('Unable to connect to database [' . $db->connect_error . ']');}
 	$db->set_charset("utf8");  
-	$statement = $db->prepare("INSERT INTO `upload` (`uploaderIP`, `reCAPTCHApass`, `imageDate`, `imageTime`, `imagePath`, `EXIFmanufacturer`, `EXIFmodel`, `gpsLat`, `gpsLong`, `gpsAlt`, `gpsTimestamp`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); 
+	$statement = $db->prepare("INSERT INTO `upload` (`uploaderIP`, `reCAPTCHApass`, `imageDate`, `imageTime`, `imagePath`, `EXIFmanufacturer`, `EXIFmodel`, `gpsLat`, `gpsLong`, `gpsAlt`, `gpsTimestamp`, `restaurantName`, `price`, `numWonton`, `rating`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); 
 	if ($statement === false){die('prepare() failed: ' . htmlspecialchars($db->error));}
-	$statement->bind_param("sisssssssds", $requestIP, $reCAPTCHApass, $date, $time, $path, $EXIFmanufacturer, $EXIFmodel, $EXIFlat, $EXIFlong, $EXIFalt, $EXIFtimestamp); 
+	$statement->bind_param("sisssssssdssdii", $requestIP, $reCAPTCHApass, $date, $time, $path, $EXIFmanufacturer, $EXIFmodel, $EXIFlat, $EXIFlong, $EXIFalt, $EXIFtimestamp, $restaurantName, $price, $numWonton, $rating); 
 	$statement->execute(); 
 ?>
